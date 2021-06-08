9907259da3ebe66806714ab572eeda462f99d33c
Amit Eshore <amiteshore@gmail.com>
2021-06-08 20:34:41 +0530

    Redesign

--- src/App.js
+++ src/App.js
@@ -1,34 +1,15 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <>
-      <header>
-        <div className="container">
-          <h1>Amit Eshore</h1>
-        </div>
-      </header>
-      <footer>
-        <div className="container">
-          <a
-            href="https://github.com/amiteshore"
-            target="_blank"
-            rel="noopener noreferrer">
-            github
-          </a>
-          {" "}&bull;{" "}
-          <a
-            href="https://twitter.com/amiteshore"
-            target="_blank"
-            rel="noopener noreferrer">
-            twitter
-          </a>
-          {" "}&bull;{" "}
-          <a 
-            href="mailto:amiteshore@gmail.com">
-            amiteshore@gmail.com
-          </a>
-        </div>
-      </footer>
-    </>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+function Home() {
+  return (
+    <main>
+      <h1>Amit Eshore</h1>
+      <p>
+        <a href="http://github.com/amiteshore">GitHub</a>
+      </p>
+      <p>
+        <a href="http://github.com/amiteshore/dotfiles">Dotfiles</a>
+      </p>
+    </main>
+  );
+}
+
+export default Home;
