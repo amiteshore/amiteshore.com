3f6f020e5ac8c7ea0ad1f47f5ff1edd0c754e4cf
Amit Eshore <amiteshore@gmail.com>
2022-04-02 22:04:57 +0530

    Move parent components to index.js from App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { HelmetProvider } from "react-helmet-async";
+import { Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import ArchLinux from "./pages/ArchLinux";
 import LampStack from "./pages/LampStack";
 
 function App() {
   return (
-    <HelmetProvider>
-      <Router>
-        <Routes>
-          <Route path="/lamp-stack" element={<LampStack />} />
-          <Route path="/arch-linux" element={<ArchLinux />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
-    </HelmetProvider>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/lamp-stack" element={<LampStack />} />
+      <Route path="/arch-linux" element={<ArchLinux />} />
+    </Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
-import React from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 
 import App from "./App";
 import "./index.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-const element = (
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-root.render(element);
+root.render(
+  <StrictMode>
+    <BrowserRouter>
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
